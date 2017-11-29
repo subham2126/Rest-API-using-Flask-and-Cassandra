0fb8a9f828a0fcb39d2f99c97fbca79c8e96ef29
Subham  Gupta <subham.gupta@medlife.com>
2017-11-29 10:42:06 +0530

    [Added api to search on the basis of id]

--- app/routes/route.py
+++ app/routes/route.py
@@ -18,7 +18,7 @@ class myClass:
                 'city' : row.emp_city,
                 'name' : row.emp_name}
             rows_as_dict.append(temp)
-            print (row.emp_id, row.emp_city, row.emp_name)
+            #print (row.emp_id, row.emp_city, row.emp_name)
         return ((json.dumps(rows_as_dict)));
     @api.route('/signup')
     def sign_up():
@@ -45,6 +45,25 @@ class myClass:
         (id, city, name)
         )
         return "SUCCESS"
+    @api.route('/search')
+    def search():
+        id_search = (request.args['x'])
+        session = cluster.connect('tutorialspoint')
+        rows = session.execute("SELECT emp_id, emp_city, emp_name FROM emp WHERE emp_id=" + id_search)
+        rows_as_dict = []
+       
+        
+        for row in rows:
+
+            temp = {
+                'id' : row.emp_id,
+                'city' : row.emp_city,
+                'name' : row.emp_name}
+            
+            rows_as_dict.append(temp)
+            
+            #print (row.emp_id, row.emp_city, row.emp_name)
+        return ((json.dumps(rows_as_dict)))
     
     
 
